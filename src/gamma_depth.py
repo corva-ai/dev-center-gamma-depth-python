f006fada62f2b6b9b86a2dcc808cb0ceaf43a952
Oleksii Symon <oleksii.symon@corva.ai>
2021-01-27 14:09:22 +0200

    added gamma_depth.py

---
+++ src/gamma_depth.py
@@ -0,0 +1,85 @@
+import itertools
+import json
+from typing import List
+
+import pydantic
+from corva import Api, Cache, StreamEvent
+
+from src.configuration import SETTINGS
+from src.models import ActualGammaDepth, ActualGammaDepthData, Drillstring, GammaDepthEvent
+
+
+def get_drillstrings(asset_id: int, ids: List[str], api: Api, limit: int) -> List[Drillstring]:
+    for skip in itertools.count(0, limit):
+        response = api.get(
+            'api/v1/data/corva/data.drillstring/',
+            params={
+                'query': json.dumps({
+                    'asset_id': asset_id,
+                    '_id': {'$in': list(ids)}
+                }),
+                'sort': '{"timestamp": 1}',
+                'limit': limit,
+                'skip': skip,
+                'fields': '_id,data'
+            }
+        )
+        drillstrings = pydantic.parse_obj_as(List[Drillstring], response.json())
+
+        yield drillstrings
+
+        if len(drillstrings) != limit:
+            break
+
+
+def gamma_depth(event: StreamEvent, api: Api, cache: Cache):
+    event = GammaDepthEvent.parse_obj(event)
+
+    event = GammaDepthEvent.filter_records_with_no_drillstring_id(event=event)
+
+    if not event.drillstring_ids:
+        return
+
+    drillstrings = list(
+        itertools.chain(
+            *get_drillstrings(
+                asset_id=event.asset_id, ids=event.drillstring_ids, api=api, limit=100
+            )
+        )
+    )
+
+    id_to_drillstring = {
+        drillstring.id: drillstring
+        for drillstring in drillstrings
+    }
+
+    actual_gamma_depths = []
+    for record in event.records:  # build actual gamma depth for each record
+        drillstring = id_to_drillstring[record.metadata.drillstring_id]  # type: Drillstring
+
+        gamma_depth_val = (
+            record.data.bit_depth - drillstring.mwd_with_gamma_sensor.gamma_sensor_to_bit_distance
+            if drillstring.mwd_with_gamma_sensor
+            else record.data.bit_depth
+        )
+
+        actual_gamma_depths.append(
+            ActualGammaDepth(
+                asset_id=record.asset_id,
+                collection=SETTINGS.collection,
+                company_id=record.company_id,
+                data=ActualGammaDepthData(
+                    gamma_depth=gamma_depth_val,
+                    bit_depth=record.data.bit_depth,
+                    gamma_ray=record.data.gamma_ray
+                ),
+                provider=SETTINGS.provider,
+                timestamp=record.timestamp,
+                version=SETTINGS.version
+            )
+        )
+
+    api.post(
+        f'api/v1/data/{SETTINGS.provider}/{SETTINGS.collection}/',
+        data=[entry.dict() for entry in actual_gamma_depths]
+    )
