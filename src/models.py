ffa8a0260410fdae7c091a456cd74d5ad2bf3eae
Oleksii Symon <oleksii.symon@corva.ai>
2021-01-27 14:07:31 +0200

    added models.py

---
+++ src/models.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+import pydantic
+from corva import StreamEvent
+from corva.models.stream import Record, RecordData
+
+
+class GammaDepthRecordMetadata(pydantic.BaseModel):
+    drillstring_id: Optional[str] = pydantic.Field(None, alias='drillstring')
+
+
+class GammaDepthRecordData(RecordData):
+    bit_depth: float
+    gamma_ray: int
+
+
+class GammaDepthRecord(Record):
+    data: GammaDepthRecordData
+    metadata: GammaDepthRecordMetadata
+
+
+class GammaDepthEvent(StreamEvent):
+    records: List[GammaDepthRecord]
+
+    @staticmethod
+    def filter_records_with_no_drillstring_id(event: GammaDepthEvent) -> GammaDepthEvent:
+        new_records = [
+            record
+            for record in event.records
+            if record.metadata.drillstring_id is not None
+        ]
+        return event.copy(update={'records': new_records}, deep=True)
+
+    @property
+    def drillstring_ids(self) -> List[str]:
+        """returns unique drillstring ids"""
+
+        ids = list(
+            set(
+                record.metadata.drillstring_id
+                for record in self.records
+            )
+        )
+
+        return ids
+
+
+class DrillstringDataComponent(pydantic.BaseModel):
+    family: str
+    gamma_sensor_to_bit_distance: float
+    has_gamma_sensor: bool
+
+
+class DrillstringData(pydantic.BaseModel):
+    components: List[DrillstringDataComponent]
+
+
+class Drillstring(pydantic.BaseModel):
+    """Needed subset of drillstring response fields"""
+
+    id: str = pydantic.Field(..., alias='_id')
+    data: DrillstringData
+
+    @property
+    def mwd_with_gamma_sensor(self) -> Optional[DrillstringDataComponent]:
+        """returns MWD component with a gamma sensor"""
+
+        for component in self.data.components:
+            if component.family == 'mwd' and component.has_gamma_sensor:
+                return component
+
+        return None
+
+
+class ActualGammaDepthData(pydantic.BaseModel):
+    bit_depth: float
+    gamma_depth: float
+    gamma_ray: int
+
+
+class ActualGammaDepth(pydantic.BaseModel):
+    asset_id: int
+    collection: str
+    company_id: int
+    data: ActualGammaDepthData
+    provider: str
+    timestamp: int
+    version: int
